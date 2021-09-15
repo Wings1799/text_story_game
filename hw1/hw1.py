a168e34950ec0086c0159d1fd9292c66f1249053
Max Winge <winge1@stolaf.edu>
2021-09-15 11:39:06 -0500

    new structure

---
+++ hw1/hw1.py
@@ -0,0 +1,33 @@
+a=2
+b=3
+while a < 10:
+    print(a)
+    a, b = b, a+b
+
+i=256*256
+print('The value of i is ', i)
+
+line = 'this is my life'
+end = ' and I want to enjoy it'
+print(line+end)
+
+x=3
+y=6
+while y>3:
+   print(x+y)
+   y-=1
+
+
+a='my friends: '
+b='bravo '
+c='charlie '
+d='delta '
+print(a + b + c + d)
+
+#MINI PROJECT IDEAS
+#1. interactive story
+#2. sports stat tracker
+#3. gpa calculator
+#4. homework log
+#5. personal calendar
+#6. calorie/food tracker
