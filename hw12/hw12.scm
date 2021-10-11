4eb24ab141588538934d72515e7b3c61feec1eee
Max Winge <winge1@stolaf.edu>
2021-10-10 21:36:52 -0500

    hw12 experiments in Scheme

---
+++ hw12/hw12.scm
@@ -0,0 +1,20 @@
+(+ 8 3)
+
+(define name 
+    (car '(jim 17 fireman)))
+
+(define age
+    (car '(17 fireman)))
+
+(define occupation
+    (car '(fireman)))
+
+(define triple
+    (lambda (n)
+        (* n 3)))
+
+(define string
+    (quote ("Hello" "World")))
+
+(define profile
+    (cons 'james '()))
